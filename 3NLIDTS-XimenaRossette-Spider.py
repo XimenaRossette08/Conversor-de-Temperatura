486be2b2dcdfe07ec82598deb78e5e44a3488267
XimenaRossette08 <luz.rossette79@unach.mx>
2024-08-21 18:10:51 -0600

    Add files via upload

---
+++ 3NLIDTS-XimenaRossette-Spider.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+from tkinter import *
+ventana = Tk()
+ventana.title("Primera Ventana en Python Soy Ximeeeeeeeeeeeee")
+ventana.geometry("520x480")
+ventana.mainloop()
